7576d918eb5e6792292b7b97ad32d5b37798661b
Kavitha <kavitha.m@stackroute.in>
2022-10-20 12:02:16 +0530

    control structures and pop-ups

---
+++ Javascript/script2.js
@@ -0,0 +1,11 @@
+// pop - up
+
+// var alert=alert("Please upload the photo of size less than 100KB")
+// console.log(alert);
+
+// var data=confirm("Do you want to continue ?")
+// console.log(`The value of data is ${data}`);
+
+
+// var name=prompt("Enter your name")
+// console.log(name);
--- Javascript/session.md
+++ Javascript/session.md
@@ -101,3 +101,10 @@ true      ||        false  =     true
 true     ||       true   =     true
 false     ||        false  =     false
 false      ||       true   =     true
+
+
+Pop -up
+-------- 
+- alert 
+- confirm
+- prompt
