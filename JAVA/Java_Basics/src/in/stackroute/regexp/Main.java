d27b4694b8d7005813c4eee94e762581a5806820
Kavitha M <Kavitha.M@in.niit.com>
2022-11-28 18:07:51 +0530

    OOPS - Basics

--- JAVA/Java_Basics/src/in/stackroute/regexp/Main.java
+++ JAVA/Java_Basics/src/in/stackroute/regexp/Main.java
@@ -1,5 +1,7 @@
 package in.stackroute.regexp;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
        String company = "stackroute";
@@ -10,6 +12,10 @@ public class Main {
       String  panNumber = "ABCDE1345P";
 //        System.out.println(panNumber.matches("[A-Z]{5}[0-9]{4}[A-Z]"));
         System.out.println(panNumber.matches("[A-Z]{5}\\d{4}[A-Z]")?"Valid":"Invalid");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan);
+
 
     }
 }
--- JAVA/Java_Basics/src/in/stackroute/regexp/PatternMatcherDemo.java
+++ JAVA/Java_Basics/src/in/stackroute/regexp/PatternMatcherDemo.java
@@ -6,25 +6,35 @@ import java.util.regex.Pattern;
 
 public class PatternMatcherDemo {
     public static void main(String[] args) {
-        String text = "abc11def22x333zzzz44444";
+//        String text = "abc11def22x333zzzz44444";
+//
+//        Pattern pattern = Pattern.compile("\\d+");
+//
+//        Matcher matcher = pattern.matcher(text);
+//
+//       while (matcher.find()){
+////           System.out.println(matcher.group());
+////           System.out.print(" "+matcher.start());
+////           System.out.println(" "+matcher.end());
+//       }
+//
+//       String name = "Natwest Group";
+//        String[] splitName = name.split("t");
+//
+//        System.out.println(splitName); // address of obj
+//
+//        System.out.println(Arrays.toString(splitName));
+//
 
-        Pattern pattern = Pattern.compile("\\d+");
+        String s1 ="RAM";
+        String s2 ="RAM";
 
-        Matcher matcher = pattern.matcher(text);
-
-       while (matcher.find()){
-//           System.out.println(matcher.group());
-//           System.out.print(" "+matcher.start());
-//           System.out.println(" "+matcher.end());
-       }
-
-       String name = "Natwest Group";
-        String[] splitName = name.split("t");
-
-        System.out.println(splitName); // address of obj
-
-        System.out.println(Arrays.toString(splitName));
-
+        if (s1==s2){
+            System.out.println("References are equal");
+        }
+        else {
+            System.out.println("References are  not equal");
+        }
 
     }
 }
