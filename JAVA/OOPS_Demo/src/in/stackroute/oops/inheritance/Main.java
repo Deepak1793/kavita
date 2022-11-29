6173e3d0e6daa7e62a44af3b40cb730a98771126
Kavitha M <Kavitha.M@in.niit.com>
2022-11-29 12:16:28 +0530

    Inheritance and Encapsulation

---
+++ JAVA/OOPS_Demo/src/in/stackroute/oops/inheritance/Main.java
@@ -0,0 +1,18 @@
+package in.stackroute.oops.inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //tight coupling
+       // child ref = child obj
+        SavingsAccount santhosh = new SavingsAccount("Santhosh", 321564564, "st@gmail.com", 13465464, 232336.0);
+
+//        santhosh.deposit(2500.0);
+//        System.out.println(santhosh.getBalance());
+
+        //upcasting or loose coupling
+        //parent ref = child obj
+
+
+    }
+}
