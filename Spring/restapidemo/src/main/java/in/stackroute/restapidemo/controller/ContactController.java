8ce5168bd310b82d2f138d2fb051c7c06e02f27b
Kavitha M <Kavitha.M@in.niit.com>
2022-12-15 11:40:44 +0530

    REST API

--- Spring/restapidemo/src/main/java/in/stackroute/restapidemo/controller/ContactController.java
+++ Spring/restapidemo/src/main/java/in/stackroute/restapidemo/controller/ContactController.java
@@ -1,8 +1,12 @@
 package in.stackroute.restapidemo.controller;
 
+import in.stackroute.restapidemo.exceptions.ContactExistsException;
+import in.stackroute.restapidemo.exceptions.ContactNotFoundException;
 import in.stackroute.restapidemo.model.Contact;
 import in.stackroute.restapidemo.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,23 +21,37 @@ public class ContactController {
     private ContactRepository repository;//has-a
 
     @GetMapping("/info")
-    public String apinfo(){
+    public String apinfo() {
         return "Contact API is running";
     }
 
     @GetMapping("/newinfo")
-    public String newapifo(){
+    public String newapifo() {
         return "New Contact API running";
     }
 
     @GetMapping("/contacts")
-    public List<Contact> getAllcontacts(){
+    public List<Contact> getAllcontacts() {
         return repository.getAllContacts();
     }
 
+//    @PostMapping("/contacts")
+//    @ResponseStatus(HttpStatus.CREATED)//201
+//    public Contact addContact(@RequestBody Contact newContact) {
+//        return repository.addContact(newContact);
+//    }
+
     @PostMapping("/contacts")
-    public Contact addContact(@RequestBody Contact newContact){
-        return repository.addContact(newContact);
+    public ResponseEntity<Contact> addContact(@RequestBody Contact newContact) throws ContactExistsException {
+        Contact contact = repository.addContact(newContact);
+        return new ResponseEntity<>(contact,HttpStatus.CREATED);
     }
+
+    @DeleteMapping("/contacts/{id}")
+    public ResponseEntity<?> deleteContact(@PathVariable String id) throws ContactNotFoundException {
+        repository.deleteContact(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 
 }
--- Spring/restapidemo/src/main/java/in/stackroute/restapidemo/repository/ContactRepository.java
+++ Spring/restapidemo/src/main/java/in/stackroute/restapidemo/repository/ContactRepository.java
@@ -1,5 +1,7 @@
 package in.stackroute.restapidemo.repository;
 
+import in.stackroute.restapidemo.exceptions.ContactExistsException;
+import in.stackroute.restapidemo.exceptions.ContactNotFoundException;
 import in.stackroute.restapidemo.model.Contact;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +10,7 @@ import java.util.*;
 @Repository
 public class ContactRepository {
 
-    private Map<String, Contact> contactsRepo=new HashMap<>();
+    private Map<String, Contact> contactsRepo=new HashMap<>(); //in memory database
 
     public ContactRepository() {
        contactsRepo.put("1", new Contact("1","Anu","anu@gmail.com",132125412,"friends"));
@@ -19,12 +21,28 @@ public class ContactRepository {
 
     }
 
-    public Contact addContact(Contact contact){
+    public Contact addContact(Contact contact) throws ContactExistsException {
+
+        boolean contactExits = contactsRepo.values().stream().anyMatch(temp -> temp.getEmail().equalsIgnoreCase(contact.getEmail()));
+
+        if (contactExits){
+           throw new ContactExistsException("Contact already exists for the given email");
+        }
         String contactId = UUID.randomUUID().toString();
         contact.setContactId(contactId);
         contactsRepo.put(contact.getContactId(), contact);
         return contactsRepo.get(contactId);
 
+
+    }
+
+    public void deleteContact(String contactId) throws ContactNotFoundException {
+        Contact contact = contactsRepo.get(contactId);
+        if(contact==null){
+            throw new ContactNotFoundException("Contact with the given id does not exists");
+        }
+
+        contactsRepo.remove(contactId);
 
     }
 }
