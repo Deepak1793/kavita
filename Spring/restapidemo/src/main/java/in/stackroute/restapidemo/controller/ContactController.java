65d0baf5f1dca5bfe286adc3c1bd0411e69b5120
Kavitha M <Kavitha.M@in.niit.com>
2022-12-16 12:06:24 +0530

    REST API

--- Spring/restapidemo/src/main/java/in/stackroute/restapidemo/controller/ContactController.java
+++ Spring/restapidemo/src/main/java/in/stackroute/restapidemo/controller/ContactController.java
@@ -4,6 +4,7 @@ import in.stackroute.restapidemo.exceptions.ContactExistsException;
 import in.stackroute.restapidemo.exceptions.ContactNotFoundException;
 import in.stackroute.restapidemo.model.Contact;
 import in.stackroute.restapidemo.repository.ContactRepository;
+import in.stackroute.restapidemo.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +18,11 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class ContactController {
 
+//    @Autowired
+//    private ContactRepository repository;//has-a
+
     @Autowired
-    private ContactRepository repository;//has-a
+    private ContactService service;
 
     @GetMapping("/info")
     public String apinfo() {
@@ -32,7 +36,7 @@ public class ContactController {
 
     @GetMapping("/contacts")
     public List<Contact> getAllcontacts() {
-        return repository.getAllContacts();
+        return service.getAllContacts();
     }
 
 //    @PostMapping("/contacts")
@@ -41,17 +45,37 @@ public class ContactController {
 //        return repository.addContact(newContact);
 //    }
 
+    //Getting the contact by email - HttpStatus.OK
+    @GetMapping("/contacts/{email}")
+    public Contact getContactByEmail (@PathVariable String email) throws ContactNotFoundException {
+        return service.getContactByEmail(email);
+    }
+
+    @GetMapping(path = "/contacts", params = "category")
+    public List<Contact> getAllcontactsByCategory(@RequestParam String category){
+    return service.getAllcontactsByCategory(category);
+    }
+
+
+
     @PostMapping("/contacts")
     public ResponseEntity<Contact> addContact(@RequestBody Contact newContact) throws ContactExistsException {
-        Contact contact = repository.addContact(newContact);
+        Contact contact = service.addContact(newContact);
         return new ResponseEntity<>(contact,HttpStatus.CREATED);
     }
 
     @DeleteMapping("/contacts/{id}")
     public ResponseEntity<?> deleteContact(@PathVariable String id) throws ContactNotFoundException {
-        repository.deleteContact(id);
+        service.deleteContact(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+//    @PutMapping("/contacts/{id}")
+//    public ResponseEntity<Contact> editContact(@RequestBody Contact newContact, @PathVariable String id) throws ContactNotFoundException {
+//        Contact updatedContact = service.editContact(newContact, id);
+//        return new ResponseEntity<>(updatedContact,HttpStatus.OK);
+//    }
+
 
 
 }
--- Spring/restapidemo/src/main/java/in/stackroute/restapidemo/repository/ContactRepository.java
+++ Spring/restapidemo/src/main/java/in/stackroute/restapidemo/repository/ContactRepository.java
@@ -6,6 +6,8 @@ import in.stackroute.restapidemo.model.Contact;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Repository
 public class ContactRepository {
@@ -21,17 +23,19 @@ public class ContactRepository {
 
     }
 
-    public Contact addContact(Contact contact) throws ContactExistsException {
+    public Optional<Contact> addContact(Contact contact) throws ContactExistsException {
 
         boolean contactExits = contactsRepo.values().stream().anyMatch(temp -> temp.getEmail().equalsIgnoreCase(contact.getEmail()));
 
         if (contactExits){
-           throw new ContactExistsException("Contact already exists for the given email");
+//           throw new ContactExistsException("Contact already exists for the given email");
+            return Optional.empty();
         }
         String contactId = UUID.randomUUID().toString();
         contact.setContactId(contactId);
         contactsRepo.put(contact.getContactId(), contact);
-        return contactsRepo.get(contactId);
+        return Optional.of(contactsRepo.get(contactId));
+
 
 
     }
@@ -43,6 +47,34 @@ public class ContactRepository {
         }
 
         contactsRepo.remove(contactId);
+
+    }
+
+    public Contact getContactByEmail(String email) throws ContactNotFoundException {
+        Optional<Contact> optionalContact = contactsRepo.values().stream().filter(contact -> contact.getEmail().equalsIgnoreCase(email)).findFirst();
+        return optionalContact.orElseThrow(()->new ContactNotFoundException("Contact with the given email does not exists"));
+
+    }
+    
+    public List<Contact> getAllcontactsByCategory(String category){
+        List<Contact> contactsCategory = contactsRepo.values().stream().filter(contact -> contact.getCategory().equalsIgnoreCase(category)).collect(Collectors.toList());
+        return contactsCategory;
+
+    }
+
+    public Contact  editContact(Contact newContact,String contactId) throws ContactNotFoundException {
+        Contact contact = contactsRepo.get(contactId);
+        if(contact==null){
+            throw new ContactNotFoundException("Contact with the given id does not exists");
+        }
+
+        newContact.setContactId(contactId);
+        Contact updatedContact = contactsRepo.put(newContact.getContactId(), newContact);
+
+        return contactsRepo.get(newContact.getContactId());
+
+
+
 
     }
 }
