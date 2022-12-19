6ec084e0a53ad2de9d48b873febfff11283cfbdf
Kavitha M <Kavitha.M@in.niit.com>
2022-12-19 12:06:10 +0530

    Spring Data JPA

---
+++ Spring/userprofile/src/main/java/in/stackroute/userprofile/repository/UserProfileRepository.java
@@ -0,0 +1,7 @@
+package in.stackroute.userprofile.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserProfileRepository extends JpaRepository<---,----> {
+
+}
---
+++ Spring/userprofile/src/test/java/in/stackroute/userprofile/UserprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package in.stackroute.userprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
